04b376da11db44c59d8bb91825db876865419e1e
David Monk <david__monk@outlook.com>
2020-02-02 10:56:01 +0000

    Updates

---
+++ precache-manifest.d40c03a97ed46054b1f08881762601fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbd7e486a66592b6f719ed6f6886a3f",
+    "url": "/Deeson-React/index.html"
+  },
+  {
+    "revision": "ae9f512eb229794cf198",
+    "url": "/Deeson-React/static/css/main.10f90d0a.chunk.css"
+  },
+  {
+    "revision": "2433fec4739a18f5d57c",
+    "url": "/Deeson-React/static/js/2.2fefcdfa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Deeson-React/static/js/2.2fefcdfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9f512eb229794cf198",
+    "url": "/Deeson-React/static/js/main.3968caf0.chunk.js"
+  },
+  {
+    "revision": "17ba2b6629dcbea5bb01",
+    "url": "/Deeson-React/static/js/runtime-main.606d899a.js"
+  }
+]);
